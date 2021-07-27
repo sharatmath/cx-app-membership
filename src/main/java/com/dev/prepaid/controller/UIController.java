e471a1363272424dfe79b1eba69993ba98bc2529
Ozi Priawadi <jie.ilkom46ipb@gmail.com>
2021-07-27 11:12:21 +0700

    Integerate data using AlpineJS

--- src/main/java/com/dev/prepaid/controller/UIController.java
+++ src/main/java/com/dev/prepaid/controller/UIController.java
@@ -46,7 +46,7 @@ public class UIController {
 				.build();
 		
 		model.addAttribute("status", "CREATED");
-		model.addAttribute("instanceId", "instance-id");
+		model.addAttribute("instanceId", "63483226-49f3-48a2-8720-4f4c47021e32");
 		model.addAttribute("offers", new Config());
         return "config";
 	}
@@ -55,7 +55,7 @@ public class UIController {
     public String configure(@RequestBody ServiceInstanceConfiguration serviceInstance, Model model){
 		log.info("configure call");
 		log.info(GsonUtils.deserializeObjectToJSON(serviceInstance));
-		
+
 		try {
 			String offerBucketId = null;
 			String offerBucketCode = null;
@@ -65,13 +65,13 @@ public class UIController {
 			if(instance == null) {
 				throw new EntityNotFoundException("Instance id not found " + serviceInstance.getUuid());
 			}
-	
+
 			instance.setProgramId(serviceInstance.getAssetId());
 			prepaidCxProvInstancesRepository.save(instance);
-			
+
 			log.debug("status : {} id : {}", instance.getStatus(), serviceInstance.getUuid());
-			
-	
+
+
 			if(instance.getCampaignOfferType() != null && instance.getCampaignOfferType().equalsIgnoreCase("oms")) {
 				PrepaidOmsOfferCampaign offer = offerService.getOmsOfferCampaign(instance.getCampaignOfferId());
 				PrepaidOmsOfferBucket bucket = offerService.getOmsOfferBucket(offer.getOfferId());
@@ -79,29 +79,29 @@ public class UIController {
 				offerBucketId = "OMS|"+bucket.getId();
 				offerBucketCode = bucket.getCode();
 			}
-			
+
 			if(instance.getCampaignOfferType() != null && instance.getCampaignOfferType().equalsIgnoreCase("da")) {
 				PrepaidDaOfferCampaign offer = offerService.getDaOfferCampaign(instance.getCampaignOfferId());
 				PrepaidDaOfferBucket bucket = offerService.getDaOfferBucket(offer.getOfferId());
 				offerCampaignName = offer.getName();
 				offerBucketId = "DA|"+bucket.getId();
 				offerBucketCode = bucket.getCode();
-				
+
 			}
-			
+
 			model.addAttribute("status", instance.getStatus());
 			model.addAttribute("instanceId", instance.getInstanceId());
 			model.addAttribute("uuid", GUIDUtil.generateGUID());
 			model.addAttribute("offers", new Config().builder()
 					.offerType(instance.getCampaignOfferType())
-					
+
 					.offerBucketId(offerBucketId)
-					.offerBucketCode(offerBucketCode)				
+					.offerBucketCode(offerBucketCode)
 					.offerCampaignId(instance.getCampaignOfferId())
 					.offerCampaignName(offerCampaignName)
-					
+
 					.notification(instance.getNotification())
-					
+
 					.build());
 	        return "config";
 		}catch (Exception e) {
