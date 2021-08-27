754843c343bc618595fa4ccdf24537e00c537d45
AdeGaos <troy.adianto.kartawinata@oracle.com>
2021-08-27 08:51:26 +0700

    redemption update

--- src/main/java/com/dev/prepaid/service/RedemptionServiceImpl.java
+++ src/main/java/com/dev/prepaid/service/RedemptionServiceImpl.java
@@ -41,76 +41,186 @@ public class RedemptionServiceImpl implements RedemptionService {
 		
 		Query q= null;
 		String sql="";
-		List<PrepaidOfferMembership> membership =null;
+		List<PrepaidOfferMembership> memberships =null;
 		
-		//1.---Total Redemption Cap---
-		//1.1 Total number of redemption radio button
-		if(pCxOfferRedemption.getIsRedemptionCapOnly()) {
-			sql="select * from prepaid_offer_membership "
+		int iTotal = 0;
+		Boolean bProcess=false;
+		
+		//---Recurring---
+		if (pCxOfferRedemption.getTotalRecurringFrequency()!=null &&
+			pCxOfferRedemption.getTotalRecurringFrequency() > 0 ) {
+			
+			sql="select count(*) as counts from prepaid_offer_membership "
+				+ "where "
+				+ "offer_config_id='"+pCxOfferRedemption.getOfferConfigId()+"' and "
+				+ "msisdn ="+msisdn+" and "
+				+ "redemption_date is not null ";
+			log.info("sql="+sql);
+				
+			q=em.createNativeQuery(sql);
+			iTotal = ((Number)q.getSingleResult()).intValue();
+			
+			if (iTotal >= pCxOfferRedemption.getTotalRecurringFrequency())
+				return;
+		
+			//Recurring radion batton 1
+			if (pCxOfferRedemption.getIsRecurringFrequencyAndPeriod()) {
+				if (pCxOfferRedemption.getRecurringFrequencyPeriodType().equalsIgnoreCase("days")) {
+					sql="select count(*) as counts from prepaid_offer_membership "
+						+ "where "
+						+ "offer_config_id='"+pCxOfferRedemption.getOfferConfigId()+"' and "
+						+ "MSISDN ="+msisdn+" and "
+						+ "(redemption_date is not null and redemption_date = trunc(sysdate - "+pCxOfferRedemption.getRecurringFrequencyPeriodValue()+")) ";
+				}
+				else if (pCxOfferRedemption.getRecurringFrequencyPeriodType().equalsIgnoreCase("months")) {
+					sql="select count(*) as counts from prepaid_offer_membership "
+						+ "where "
+						+ "offer_config_id='"+pCxOfferRedemption.getOfferConfigId()+"' and "
+						+ "MSISDN ="+msisdn+" and "
+						+ "(redemption_date is not null and redemption_date = redemption_date = trunc(add_months(sysdate,-"+pCxOfferRedemption.getRecurringFrequencyPeriodValue()+"))) ";
+			}
+				log.info("sql="+sql);
+				
+				q=em.createNativeQuery(sql);
+				iTotal = ((Number)q.getSingleResult()).intValue() ;
+	
+				if (iTotal < pCxOfferRedemption.getRecurringFrequencyValue()) 
+					bProcess=true;
+				else
+					bProcess=false;
+			
+			}
+		
+			//Recurring radion batton 2
+			else if (pCxOfferRedemption.getIsRecurringFrequencyEachMonth()){
+				sql="select count(*) as counts from prepaid_offer_membership "
 					+ "where "
 					+ "offer_config_id='"+pCxOfferRedemption.getOfferConfigId()+"' and "
-					+ "redemption_date is null "
-					+ "order by offer_membership_id FETCH NEXT "+pCxOfferRedemption.getRedemptionCapValue()+" ROWS ONLY";
-			log.info("sql="+sql);
+					+ "msisdn ="+msisdn+" and "
+					+ "where to_char(redemption_date,'MON-YYYY') = to_char(sysdate, 'MON-YYYY') ";
+				log.info("sql="+sql);
+				
+				q=em.createNativeQuery(sql);
+				iTotal = ((Number)q.getSingleResult()).intValue() ;
+	
+				if (iTotal < pCxOfferRedemption.getRecurringFrequencyDayOfMonth())
+					bProcess=true;
+				else
+					bProcess=false;
+			}
+		}//---End of Recurring
+		
+		
+		if (bProcess==true) {
+			//1.---Total Redemption Cap---
+			//1.1 Total number of redemption radio button
+			if(pCxOfferRedemption.getIsRedemptionCapOnly()) {
+				sql="select count(*) as counts  from prepaid_offer_membership "
+					+ "where "
+					+ "offer_config_id='"+pCxOfferRedemption.getOfferConfigId()+"' and "
+					+ "msisdn ="+msisdn+" and "
+					+ "redemption_date is null ";
+				log.info("sql="+sql);
+				
+				q=em.createNativeQuery(sql);
+				iTotal = ((Number)q.getSingleResult()).intValue();
+				
+				if (iTotal < pCxOfferRedemption.getRedemptionCapValue())
+					bProcess=true;
+				else
+					bProcess=false;
+				
+			}// --- 1.1 Total number of redemption radio button
 			
-			q=em.createNativeQuery(sql);
-			membership = q.getResultList();
-		}
-		//1.2 Redemption and Time Period
-		else if (pCxOfferRedemption.getIsRedemptionCapAndPeriod()) {
-			if (pCxOfferRedemption.getTotalRedemptionPeriodType().equalsIgnoreCase("days")) {
-				sql="select * from prepaid_offer_membership "
+			//1.2 Redemption and Time Period
+			else if (pCxOfferRedemption.getIsRedemptionCapAndPeriod()) {
+				if (pCxOfferRedemption.getTotalRedemptionPeriodType().equalsIgnoreCase("days")) {
+					sql="select count(*) as counts  from prepaid_offer_membership "
 						+ "where "
 						+ "offer_config_id='"+pCxOfferRedemption.getOfferConfigId()+"' and "
-						+ "(redemption_date is not null and redemption_date = trunc(sysdate - "+pCxOfferRedemption.getTotalRedemptionPeriodEvery()+")) "
-						+ "order by offer_membership_id FETCH NEXT "+pCxOfferRedemption.getRedemptionCapValue()+" ROWS ONLY";
-			}
-			else if (pCxOfferRedemption.getTotalRedemptionPeriodType().equalsIgnoreCase("months")) {
-				sql="select * from prepaid_offer_membership "
+						+ "msisdn ="+msisdn+" and "
+						+ "(redemption_date is not null and redemption_date = trunc(sysdate - "+pCxOfferRedemption.getTotalRedemptionPeriodEvery()+"))";
+				}
+				else if (pCxOfferRedemption.getTotalRedemptionPeriodType().equalsIgnoreCase("months")) {
+					sql="select * from prepaid_offer_membership "
 						+ "where "
 						+ "offer_config_id='"+pCxOfferRedemption.getOfferConfigId()+"' and "
-						+ "(redemption_date is not null and redemption_date = redemption_date = trunc(add_months(sysdate,-"+pCxOfferRedemption.getTotalRedemptionPeriodEvery()+"))) "
-						+ "order by offer_membership_id FETCH NEXT "+pCxOfferRedemption.getRedemptionCapValue()+" ROWS ONLY";
-			}
-			log.info("sql="+sql);
+						+ "msisdn ="+msisdn+" and "
+						+ "(redemption_date is not null and redemption_date = redemption_date = trunc(add_months(sysdate,-"+pCxOfferRedemption.getTotalRedemptionPeriodEvery()+")))";
+				}
+				log.info("sql="+sql);
+				
+				q=em.createNativeQuery(sql);
+				iTotal = ((Number)q.getSingleResult()).intValue() ;
+
+				if (iTotal < pCxOfferRedemption.getRedemptionCapValue() ) 
+					bProcess=true;
+				else
+					bProcess=false;
+			} // --- End of 1.2 Redemption and Time Period
 			
-			q=em.createNativeQuery(sql);
-			membership = q.getResultList();
+			
+			//2.---Subsricber Redemption Cap---
+			if(pCxOfferRedemption.getIsFrequencyOnly()) {
+				sql="select * from prepaid_offer_membership "
+					+ "where "
+					+ "offer_config_id='"+pCxOfferRedemption.getOfferConfigId()+"' and "
+					+ "msisdn ="+msisdn+" and "
+					+ "redemption_date is null ";
+				log.info("sql="+sql);
+				
+				q=em.createNativeQuery(sql);
+				iTotal = ((Number)q.getSingleResult()).intValue() ;
+
+				if (iTotal < pCxOfferRedemption.getFrequencyValue() ) 
+					bProcess=true;
+				else
+					bProcess=false;
+			}
+			else if (pCxOfferRedemption.getIsFrequencyAndTime()) {
+				if (pCxOfferRedemption.getTimePeriodType().equalsIgnoreCase("days")) {
+					sql="select * from prepaid_offer_membership "
+						+ "where "
+						+ "offer_config_id='"+pCxOfferRedemption.getOfferConfigId()+"' and "
+						+ "msisdn ="+msisdn+" and "
+						+ "(redemption_date is not null and redemption_date = trunc(sysdate - "+pCxOfferRedemption.getTotalRedemptionPeriodEvery()+"))";
+				}
+				else if (pCxOfferRedemption.getTimePeriodType().equalsIgnoreCase("months")) {
+					sql="select * from prepaid_offer_membership "
+						+ "where "
+						+ "offer_config_id='"+pCxOfferRedemption.getOfferConfigId()+"' and "
+						+ "msisdn ="+msisdn+" and "
+						+ "(redemption_date is not null and redemption_date = redemption_date = trunc(add_months(sysdate,-"+pCxOfferRedemption.getTotalRedemptionPeriodEvery()+"))) ";
+				}
+				log.info("sql="+sql);
+				
+				q=em.createNativeQuery(sql);
+				iTotal = ((Number)q.getSingleResult()).intValue() ;
+
+				if (iTotal < pCxOfferRedemption.getRedemptionCapValue() ) 
+					bProcess=true;
+				else
+					bProcess=false;
+			}
+			
+		} // --- End of bProcess
+		
+		
+		
+		if (bProcess==true) {
+			if(pCxOfferRedemption.getRedemptionMethod().equalsIgnoreCase("manual")) {
+				log.info("Process with Manual, for msisdn:"+msisdn+" call API Responsys ");
+			}
+			else if(pCxOfferRedemption.getRedemptionMethod().equalsIgnoreCase("auto")) {
+				log.info("Process with Auto, for msisdn:"+msisdn+" call API Provisoning ");
+			}
 		}
+
 		
-		if (membership.size()==0)
-			return;
-		
-		
-		
-		//2.---Subsricber Redemption Cap---
-		
-//		prepaidOfferMembershipRepository.find
 	}
 	 
 
-//	@Autowired
-//	private PrepaidOmsOfferBucketRepository prepaidOmsOfferBucketRepository;
-//	@Autowired
-//	private PrepaidOmsOfferCampaignRepository prepaidOmsOfferCampaignRepository;
-//	@Autowired
-//	private PrepaidDaOfferBucketRepository prepaidDaOfferBucketRepository;
-//	@Autowired
-//	private PrepaidDaOfferCampaignRepository prepaidDaOfferCampaignRepository;
-//	@Autowired
-//	private PrepaidCxOfferConfigRepository prepaidCxOfferConfigRepository;
-//	@Autowired
-//	private PrepaidCxOfferSelectionRepository prepaidCxOfferSelectionRepository;
-//	@Autowired
-//	private PrepaidCxOfferEligibilityRepository  prepaidCxOfferEligibilityRepository;
-//	@Autowired
-//	private PrepaidCxOfferMonitoringRepository prepaidCxOfferMonitoringRepository;
-//	@Autowired
-//	private PrepaidCxOfferRedemptionRepository prepaidCxOfferRedemptionRepository;
-//	@Autowired
-//	private PrepaidCxOfferEventConditionRepository prepaidCxOfferEventConditionRepository;
-//	@Autowired
-//	private CountryRepository countryRepository;
+ 
 
 	
 
