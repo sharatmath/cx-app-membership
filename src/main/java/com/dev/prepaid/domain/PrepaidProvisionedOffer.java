d618408fa664004eb86b164a155c046eb2b3380b
Irfan_Saputra <sb@example.com>
2021-10-06 15:09:03 +0700

    sprint2

--- src/main/java/com/dev/prepaid/domain/PrepaidProvisionedOffer.java
+++ src/main/java/com/dev/prepaid/domain/PrepaidProvisionedOffer.java
@@ -18,28 +18,12 @@ import java.util.Date;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class PrepaidProvisionedOffer implements Serializable {
+public class PrepaidProvisionedOffer extends Auditable implements Serializable {
     @Id
     @Column(name = "PROVISIONED_OFFER_ID")
     private Long id;
     private Long offerProvisionId;
-    private Long offerId;
-    private String name;
-    private String description;
-    private Long value;
-    private String valueUnit;
-    private Long valueCap;
-    private Long validity;
-    private Long valueToDeductFromMa;
-    private Date startDate;
-    private Date endDate;
-    private String action;
-    private String counterId;
-    private Long counterValue;
-    private String thresholdId;
-    private Long thresholdValue;
-    private String thresholdValueUnit;
-    private Integer day;
-    private Integer hour;
-    private Integer minute;
+    private Long offerSelectionId;
+    private Date offerDate;
+
 }
