bde457457908831954ece35fa99f4bd1ff369ede
Hereza Liem <herezaardhitya@gmail.com>
2021-11-09 12:35:22 +0900

    DLQ Message Queue

--- src/main/java/com/dev/prepaid/config/AmqpConfig.java
+++ src/main/java/com/dev/prepaid/config/AmqpConfig.java
@@ -40,8 +40,8 @@ public class AmqpConfig {
 	public Queue queueSingtelRedemption() {
 			Map<String, Object> args = new HashMap<String, Object>();
 			args.put("x-max-priority", 1);
-			args.put("x-dead-letter-exchange", "singtelRedemptionDlx");
-			args.put("x-dead-letter-routing-key", "singtelRedemptionRK");
+			args.put("x-dead-letter-exchange", "singtel.redemption.dlx");
+			args.put("x-dead-letter-routing-key", "singtel.redemption.rk");
 			Queue queue = new Queue(Constant.QUEUE_NAME_SINGTEL_REDEMPTION, true, false, false, args);
 			return queue;
 		}
@@ -71,12 +71,12 @@ public class AmqpConfig {
 
 	@Bean
 	public DirectExchange dlxSingtelRedemption() {
-		return new DirectExchange("singtelRedemptionDlx");
+		return new DirectExchange("singtel.redemption.dlx");
 	}
 
 	@Bean
 	public Binding singtelRedemptionDlqBinding() {
-		return BindingBuilder.bind(dlqSingtelRedemption()).to(dlxSingtelRedemption()).with("singtelRedemptionRK");
+		return BindingBuilder.bind(dlqSingtelRedemption()).to(dlxSingtelRedemption()).with("singtel.redemption.rk");
 	}
 
 	//============================================================================= //
@@ -85,6 +85,8 @@ public class AmqpConfig {
 	public Queue queueOfferMonitoring() {
 		Map<String, Object> args = new HashMap<String, Object>();
 		args.put("x-max-priority", 1);
+		args.put("x-dead-letter-exchange", "offer.monitoring.dlx");
+		args.put("x-dead-letter-routing-key", "offer.monitoring.rk");
 		Queue queue = new Queue(Constant.QUEUE_NAME_MEMBERSHIP_MONITORING, true, false, false, args);
 		return queue;
 	}
@@ -99,12 +101,24 @@ public class AmqpConfig {
 		return BindingBuilder.bind(queueOfferMonitoring).to(exchangeMembership)
 				.with(Constant.QUEUE_NAME_MEMBERSHIP_MONITORING); //route.key.name=queue.name
 	}
+
+	@Bean
+	public DirectExchange dlxOfferMonitoring() {
+		return new DirectExchange("offer.monitoring.dlx");
+	}
+
+	@Bean
+	public Binding offerMonitoringDlqBinding() {
+		return BindingBuilder.bind(dlqOfferMonitoring()).to(dlxOfferMonitoring()).with("offer.monitoring.rk");
+	}
 	//============================================================================= //
 	// Subscriber
 	@Bean
 	public Queue queueMembershipOfferSubscriber() {
 		Map<String, Object> args = new HashMap<String, Object>();
 		args.put("x-max-priority", 1);
+		args.put("x-dead-letter-exchange", "offer.subscriber.dlx");
+		args.put("x-dead-letter-routing-key", "offer.subscriber.rk");
 		Queue queue = new Queue(Constant.QUEUE_NAME_MEMBERSHIP_SUBSCRIBER, true, false, false, args);
 		return queue;
 	}
@@ -112,6 +126,16 @@ public class AmqpConfig {
 	@Bean
 	public Queue dlqMembershipOfferSubscriber() {
 		return new Queue(Constant.QUEUE_NAME_DLQ_MEMBERSHIP_SUBSCRIBER);
+	}
+
+	@Bean
+	public DirectExchange dlxOfferSubscriber() {
+		return new DirectExchange("offer.subscriber.dlx");
+	}
+
+	@Bean
+	public Binding offerSubscriberDlqBinding() {
+		return BindingBuilder.bind(dlqMembershipOfferSubscriber()).to(dlxOfferSubscriber()).with("offer.subscriber.rk");
 	}
 
 	@Bean
@@ -125,6 +149,8 @@ public class AmqpConfig {
 	public Queue queueMembershipOfferEligibility() {
 		Map<String, Object> args = new HashMap<String, Object>();
 		args.put("x-max-priority", 1);
+		args.put("x-dead-letter-exchange", "membership.offer.dlx");
+		args.put("x-dead-letter-routing-key", "membership.offer.rk");
 		Queue queue = new Queue(Constant.QUEUE_NAME_MEMBERSHIP_ELIGIBILITY, true, false, false, args);
 		return queue;
 	}
@@ -132,6 +158,16 @@ public class AmqpConfig {
 	@Bean
 	public Queue dlqMembershipOfferEigibility() {
 		return new Queue(Constant.QUEUE_NAME_DLQ_MEMBERSHIP_ELIGIBILITY);
+	}
+
+	@Bean
+	public DirectExchange dlxMembershipOfferEigibility() {
+		return new DirectExchange("membership.offer.dlx");
+	}
+
+	@Bean
+	public Binding membershipOfferEigibilityDlqBinding() {
+		return BindingBuilder.bind(dlqMembershipOfferEigibility()).to(dlxMembershipOfferEigibility()).with("membership.offer.rk");
 	}
 
 	@Bean
