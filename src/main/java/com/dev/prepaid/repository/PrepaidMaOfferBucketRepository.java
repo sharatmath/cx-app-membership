a8997662976f3adfeea5e9bb5ae22266811e5ac5
Hereza Liem <herezaardhitya@gmail.com>
2021-11-15 18:06:11 +0900

    ma credit CRUD

--- src/main/java/com/dev/prepaid/repository/PrepaidMaOfferBucketRepository.java
+++ src/main/java/com/dev/prepaid/repository/PrepaidMaOfferBucketRepository.java
@@ -2,6 +2,7 @@ package com.dev.prepaid.repository;
 
 import com.dev.prepaid.domain.PrepaidDaOfferBucket;
 import com.dev.prepaid.domain.PrepaidMaCreditOffer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,5 +10,7 @@ import java.util.List;
 public interface PrepaidMaOfferBucketRepository extends CrudRepository<PrepaidMaCreditOffer, Long> {
     List<PrepaidMaCreditOffer> findAll();
     PrepaidMaCreditOffer findOneById(Long id);
+
+    @Query("select ma from PrepaidMaCreditOffer ma where ma.productName = :code")
     PrepaidMaCreditOffer findOneByCode(String code);
 }
