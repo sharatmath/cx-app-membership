da43f3cec7d3a5084c2fc02e6d797807ce044191
Irfan_Saputra <sb@example.com>
2021-11-23 18:40:13 +0700

    fixing code

--- src/main/java/com/dev/prepaid/service/PrepaidEvaluationService.java
+++
@@ -1,8 +0,0 @@
-package com.dev.prepaid.service;
-
-import com.dev.prepaid.domain.PrepaidCxOfferConfig;
-import com.dev.prepaid.type.EventType;
-
-public interface PrepaidEvaluationService {
-    void evaluationPolicy(String msisdn, EventType eventType, PrepaidCxOfferConfig prepaidCxOfferConfig, String id);
-}
