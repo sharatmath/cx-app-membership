a7605ffef333fdc64faad69d068545d5a2ab955d
Hereza Liem <herezaardhitya@gmail.com>
2021-12-09 18:04:35 +0900

    fix

--- src/main/java/com/dev/prepaid/constant/Constant.java
+++ src/main/java/com/dev/prepaid/constant/Constant.java
@@ -5,7 +5,6 @@ public class Constant {
   public static final String QUEUE_NAME_MEMBERSHIP_MONITORING = "membership.offer.monitoring.queue";
   public static final String QUEUE_NAME_DLQ_MEMBERSHIP_MONITORING = "membership.dlq.monitoring.queue";
   public static final String QUEUE_NAME_MEMBERSHIP_EVENT_CONDITION = "membership.offer.event.condition.queue";
-  public static final String QUEUE_NAME_DLQ_MEMBERSHIP_MONITORING = "membership.dlq.monitoring.queue";
   public static final String QUEUE_NAME_MEMBERSHIP_SUBSCRIBER = "membership.offer.subscriber.queue";
   public static final String QUEUE_NAME_DLQ_MEMBERSHIP_SUBSCRIBER = "membership.dlq.subscriber.queue";
   public static final String QUEUE_NAME_SINGTEL_REDEMPTION = "singtel.redemption.queue";
@@ -13,7 +12,6 @@ public class Constant {
   public static final String QUEUE_NAME_MEMBERSHIP_ELIGIBILITY = "membership.offer.eligibility.queue";
   public static final String QUEUE_NAME_DLQ_MEMBERSHIP_ELIGIBILITY = "membership.dlq.eligibility.queue";
   public static final String QUEUE_NAME_SINGTEL_RESPONSYS_CUSTOM_EVENT = "singtel.prepaid.custom.event.queue";
-  public static final String QUEUE_NAME_DLQ_MEMBERSHIP_ELIGIBILITY = "membership.dlq.eligibility.queue";
 
 
   public static final String CONNECTION_FACTORY_NAME_MEMBERSHIP_MONITORING = "rabbitListenerContainerFactoryForOfferMonitoring";
