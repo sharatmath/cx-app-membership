7050d48b8aa8779511df7f4da41c68d1cfd85e78
Hereza Liem <herezaardhitya@gmail.com>
2022-02-09 18:34:05 +0900

    fix

--- src/main/java/com/dev/prepaid/repository/PrepaidCxOfferAdvanceFilterRepository.java
+++ src/main/java/com/dev/prepaid/repository/PrepaidCxOfferAdvanceFilterRepository.java
@@ -3,10 +3,12 @@ package com.dev.prepaid.repository;
 import com.dev.prepaid.domain.PrepaidCxOfferAdvanceFilter;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PrepaidCxOfferAdvanceFilterRepository extends CrudRepository<PrepaidCxOfferAdvanceFilter, Long> {
     Optional<PrepaidCxOfferAdvanceFilter> findByOfferConfigId(@Param("offerConfigId") String offerConfigId);
 }
