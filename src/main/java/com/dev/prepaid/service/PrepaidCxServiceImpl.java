46c11509c8effe076173acbeca3cc06837063a9e
Hereza Liem <herezaardhitya@gmail.com>
2022-02-14 13:41:30 +0900

    fix

--- src/main/java/com/dev/prepaid/service/PrepaidCxServiceImpl.java
+++ src/main/java/com/dev/prepaid/service/PrepaidCxServiceImpl.java
@@ -838,8 +838,8 @@ public class PrepaidCxServiceImpl implements PrepaidCxService {
 						.isCustomQuery(saveConfigRequest.getPayload().getPrepaidCxOfferAdvanceFilter().isCustomQuery())
 						.payload(payload)
 						.queryText(saveConfigRequest.getPayload().getPrepaidCxOfferAdvanceFilter().getQueryText())
-						.instanceId(offerConfigId)
-//						.offerConfigId(offerConfigId)
+						.instanceId(saveConfigRequest.getPayload().getUuid())
+						.offerConfigId(offerConfigId)
 						.isCustomQuery(saveConfigRequest.getPayload().getPrepaidCxOfferAdvanceFilter().isCustomQuery())
 						.build();
 			}
